139dbdbf70852a0422ea1aae8f517d9e07723a30
Jim Pivarski <pivarski@jim-ubuntu1204.(none)>
2012-09-27 12:41:26 -0500

    All CONFIG.ini options, README.txt examples, and build.xml dependencies have been corrected for a SequenceFile-based map-reduce job on the main system.

--- lib/serialization-sequencefile/tests/test2.py
+++ lib/serialization-sequencefile/tests/test2.py
@@ -1,22 +1,22 @@
 import jpype
 
-classpath = "matsuSequenceFileInterface.jar"
+classpath = "../matsuSequenceFileInterface.jar"
 jvmpath = "/usr/lib/jvm/java-6-sun/jre/lib/amd64/server/libjvm.so"
 
 jpype.startJVM(jvmpath, "-Djava.class.path=%s" % classpath)
 SequenceFileInterface = jpype.JClass("org.occ.matsu.SequenceFileInterface")
 
-# SequenceFileInterface.openForWriting("test.sequencefile")
-# for x in xrange(256):
-#     SequenceFileInterface.write(str(x), chr(x) + chr(x) + chr(x) + chr(x) + chr(x))
-# SequenceFileInterface.closeWriting()
+SequenceFileInterface.openForWriting("test.sequencefile", True)
+for x in xrange(256):
+    SequenceFileInterface.write(str(x), chr(x) + chr(x) + chr(x) + chr(x) + chr(x))
+SequenceFileInterface.closeWriting()
 
-# jpype.shutdownJVM()
+jpype.shutdownJVM()
 
-SequenceFileInterface.openForReading("test.sequencefile", ["22", "15", "55", "6", "99", "75", "0"])
+# SequenceFileInterface.openForReading("test.sequencefile", ["22", "15", "55", "6", "99", "75", "0"])
 
-while True:
-    result = SequenceFileInterface.readNext()
-    if result is None: break
-    key, value = result
-    print key, [ord(x) for x in value], ">%s<" % value
+# while True:
+#     result = SequenceFileInterface.readNext()
+#     if result is None: break
+#     key, value = result
+#     print key, [ord(x) for x in value], ">%s<" % value
